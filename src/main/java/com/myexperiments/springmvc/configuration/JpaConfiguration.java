a4ed9bf186fd922313b2e705e145302b26ec0a6a
HiMyNameIsIlNano <kappei84@gmail.com>
2018-11-21 14:34:36 +0100

    DM: Added Comment to JPA Config Class; Cleaned up POM File;

--- src/main/java/com/myexperiments/springmvc/configuration/JpaConfiguration.java
+++ src/main/java/com/myexperiments/springmvc/configuration/JpaConfiguration.java
@@ -3,6 +3,9 @@ package com.myexperiments.springmvc.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/*
+* Enabled Spring Data’s automatic JPA repository generation from JpaRepository Interface
+**/
 @Configuration
 @EnableJpaRepositories(basePackages="com.myexperiments.springmvc.domain")
 public class JpaConfiguration {
