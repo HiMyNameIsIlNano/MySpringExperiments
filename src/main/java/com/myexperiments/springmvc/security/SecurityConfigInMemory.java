1398ba05f95b0ea9031f44f183a4095d0adffe94
HiMyNameIsIlNano <kappei84@gmail.com>
2018-11-24 13:47:32 +0100

    DM: Fixed Issue with _csrf Attribute when Uploading Data Via Multipart-Data;

--- src/main/java/com/myexperiments/springmvc/security/SecurityConfigInMemory.java
+++ src/main/java/com/myexperiments/springmvc/security/SecurityConfigInMemory.java
@@ -1,6 +1,7 @@
 package com.myexperiments.springmvc.security;
 
 import com.myexperiments.springmvc.security.condition.InMemoryCondition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.servlet.support.csrf.CsrfRequestDataValueProcessor;
+import org.springframework.web.servlet.support.RequestDataValueProcessor;
 
 @Configuration
 @EnableWebSecurity
@@ -37,31 +40,31 @@ public class SecurityConfigInMemory extends WebSecurityConfigurerAdapter {
      * */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        http.authorizeRequests()
+                .antMatchers("/spitters/me").hasRole("SPITTER") // /spitters/me can only be accessed by Users with role SPITTER. The ROLE_ prefix is automatically prepended.
+                .antMatchers(HttpMethod.POST, "/spittles").authenticated() // POST to /spittles must be authenticated
+                .anyRequest().permitAll()
+            .and()
+                .formLogin()
                 .loginPage("/login") // It enables the default login page
                 .successForwardUrl("/home")
-                .and()
-                    /*
-                    * The following configuration demonstrates how to allow token based remember me authentication.
-                    * Upon authentication if the HTTP parameter named "remember-me" is sent, the user will be remembered
-                    * for a period of time given by the validityInSeconds parameter.
-                    * */
-                    .rememberMe()
-                    .tokenValiditySeconds(2419200) // Valid for 4 Weeks
-                    .key("spittrKey") // The password used to has the User's password into a cookie
-                .and()
-                    .logout()
-                    .logoutSuccessUrl("/")
-                    .logoutUrl("/signout")
-                .and()
-                    .authorizeRequests()
-                    .antMatchers("/spitters/me").hasRole("SPITTER") // /spitters/me can only be accessed by Users with role SPITTER. The ROLE_ prefix is automatically prepended.
-                    .antMatchers(HttpMethod.POST, "/spittles").authenticated() // POST to /spittles must be authenticated
-                    .anyRequest().permitAll()
-                .and()
-                    .requiresChannel() // Use HTTPS
-                    .antMatchers("/spitter/form")
-                    .requiresSecure(); // all the other request are not authenticated
+            .and()
+                /*
+                * The following configuration demonstrates how to allow token based remember me authentication.
+                * Upon authentication if the HTTP parameter named "remember-me" is sent, the user will be remembered
+                * for a period of time given by the validityInSeconds parameter.
+                * */
+                .rememberMe()
+                .tokenValiditySeconds(2419200) // Valid for 4 Weeks
+                .key("spittrKey") // The password used to has the User's password into a cookie
+            .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .logoutUrl("/signout")
+            .and()
+                .requiresChannel() // Use HTTPS
+                .antMatchers("/spitter/form")
+                .requiresSecure(); // all the other request are not authenticated
     }
 
 }
