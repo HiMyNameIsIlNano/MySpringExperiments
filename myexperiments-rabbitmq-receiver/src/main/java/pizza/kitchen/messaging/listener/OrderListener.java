553d5b8e24e55214d5d688bcbdab6bdd500d5787
HiMyNameIsIlNano <kappei84@gmail.com>
2019-01-16 05:47:04 +0100

    DM: Added Short Comment to RabbitListener Annotation in RabbitMQ Experiment;

--- myexperiments-rabbitmq-receiver/src/main/java/pizza/kitchen/messaging/listener/OrderListener.java
+++ myexperiments-rabbitmq-receiver/src/main/java/pizza/kitchen/messaging/listener/OrderListener.java
@@ -11,17 +11,20 @@ import pizza.kitchen.KitchenUI;
 @Profile("rabbitmq-listener")
 @Component
 public class OrderListener {
-  
-  private KitchenUI ui;
 
-  @Autowired
-  public OrderListener(KitchenUI ui) {
-    this.ui = ui;
-  }
+    private KitchenUI ui;
 
-  @RabbitListener(queues = "pizzacloud.order.queue")
-  public void receiveOrder(Order order) {
-    ui.displayOrder(order);
-  }
-  
+    @Autowired
+    public OrderListener(KitchenUI ui) {
+        this.ui = ui;
+    }
+
+    /**
+     * Listen for order messages rather than to poll for them.
+     */
+    @RabbitListener(queues = "pizzacloud.order.queue")
+    public void receiveOrder(Order order) {
+        ui.displayOrder(order);
+    }
+
 }
